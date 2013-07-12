2d6d8838c3503299a8a7a755c1cb8943fd5bb9ac
Aidan Feldman <aidan.feldman@gmail.com>
2013-07-12 04:42:57 -0400

    test (and pass) method compatibility between the gem and js

--- geo_formatter.js
+++ geo_formatter.js
@@ -18,6 +18,7 @@
     this.result = result;
   };
 
+
   geoFormatter.prototype.getCoordinates = function(){
     var loc = this.result.geometry.location;
     return [loc.lat(), loc.lng()];
@@ -27,41 +28,99 @@
     return this.result.formatted_address;
   };
 
+  geoFormatter.prototype.getNeighborhood = function(){
+    return this.matchingTypes(['neighborhood']);
+  };
+
   geoFormatter.prototype.getCity = function(){
-    return this.matchingTypes({
-      locality: true,
-      sublocality: true,
-      administrative_area_level_3: true,
-      administrative_area_level_2: true
-    }, true);
+    return this.matchingTypes([
+      'locality',
+      'sublocality',
+      'administrative_area_level_3',
+      'administrative_area_level_2'
+    ], true);
   };
 
   geoFormatter.prototype.getState = function(){
-    return this.matchingTypes({ administrative_area_level_1: true });
+    return this.matchingTypes(['administrative_area_level_1']);
   };
 
   geoFormatter.prototype.getStateCode = function(){
-    return this.matchingTypes({ administrative_area_level_1: true }, true);
+    return this.matchingTypes(['administrative_area_level_1'], true);
+  };
+
+  geoFormatter.prototype.getSubState = function(){
+    return this.matchingTypes(['administrative_area_level_2']);
+  };
+
+  geoFormatter.prototype.getSubStateCode = function(){
+    return this.matchingTypes(['administrative_area_level_2'], true);
   };
 
   geoFormatter.prototype.getCountry = function(){
-    return this.matchingTypes({ country: true });
+    return this.matchingTypes(['country']);
   };
+
+  geoFormatter.prototype.getCountryCode = function(){
+    return this.matchingTypes(['country'], true);
+  };
+
+  geoFormatter.prototype.getPostalCode = function(){
+    return this.matchingTypes(['postal_code']);
+  };
+
+  geoFormatter.prototype.getRoute = function(){
+    return this.matchingTypes(['route']);
+  };
+
+  geoFormatter.prototype.getStreetNumber = function(){
+    return this.matchingTypes(['street_number']);
+  };
+
+  geoFormatter.prototype.getStreetAddress = function(){
+    var streetNum = this.getStreetNumber(),
+      route = this.getRoute();
+
+    if (streetNum && route){
+      return [streetNum, route].join(' ');
+    } else {
+      return streetNum || route;
+    }
+  };
+
+  geoFormatter.prototype.getTypes = function(){
+    return this.result.types;
+  };
+
+  geoFormatter.prototype.getFormattedAddress = function(){
+    return this.result.formatted_address;
+  };
+
+  geoFormatter.prototype.getGeometry = function(){
+    return this.result.geometry;
+  };
+
+  geoFormatter.prototype.getPrecision = function(){
+    var geom = this.getGeometry();
+    return geom ? geom.location_type : null;
+  };
+
 
   geoFormatter.prototype.matchingTypes = function(types, shortName){
     var components = this.result.address_components,
-      component, compTypes, i, j;
+      type, component, i, j;
 
-    for (i = 0; i < components.length; i++){
-      component = components[i];
-      compTypes = component.types;
-      for (j = 0; j < compTypes.length; j++){
-        if (types[compTypes[j]]){
+    for (i = 0; i < types.length; i++){
+      type = types[i];
+      for (j = 0; j < components.length; j++){
+        component = components[j];
+        if (component.types.indexOf(type) > -1){
           return shortName ? component.short_name : component.long_name;
         }
       }
     }
   };
+
 
   return geoFormatter;
 }));
